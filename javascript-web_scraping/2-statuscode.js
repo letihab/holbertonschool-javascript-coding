852e96af9ff7822d46c15022684ab00f6da0b1f7
letihab <letihabib1822@gmail.com>
2024-01-24 16:46:25 +0100

    update task 2 and add style

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -5,15 +5,10 @@ const request = require('request');
 
 const url = process.argv[2];
 
-if (!url) {
-    console.error("Usage: node 2-statuscode.js <url>");
-    process.exit(1);
-}
-
 request.get(url, (error, response) => {
-    if (error) {
-        console.error(error);
-    } else {
-        console.log('code: ${response.statusCode}');
-    }
+  if (error) {
+    console.error(`Error:${error.message}`);
+  } else {
+    console.log('code:', response.statusCode);
+  }
 });
