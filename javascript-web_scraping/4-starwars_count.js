7449f617a89d65de9e8ea3ca7562ae444ea29bf8
letihab <letihabib1822@gmail.com>
2024-01-24 17:04:04 +0100

    update task 2 and add style

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -4,25 +4,23 @@
 const request = require('request');
 
 const apiurl = process.argv[2];
-
-if(!apiurl) {
-    console.error("Usage: node 4-starwars_count.js <api_url>");
-    process.exit(1);
-}
-
 const wedgeAntillesId = 18;;
 
 request(apiurl, (error, response, body) => {
-    if (error) {
-        console.error(error);
-    } else if (response.statusCode === 200) {
-        const filmsData = JSON.parse(body);
-        const filmsWithWedgeAntilles = filmsData.results.filter(film =>
-            film.characters.includes(`https://swapi-api.hbtn.io/api/people/${wedgeAntillesId}/`)
-        );
+  if (error) {
+    console.error(error);
+  } 
+  else {
+    const filmData = JSON.parse(body).results;
+    let numberApparition = 0;
 
-        console.log(filmsWithWedgeAntilles.length);
-    } else {
-        console.error(`Error: ${response.statusCode}`);
+  for (const film of filmData) {
+    for (const character of film.characters) {
+      if (character.includes('/18/')) {
+        numberApparition += 1;
+      }
     }
+  }
+    console.log(numberApparition);
+  }
 });
--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -7,23 +7,16 @@ const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-if (!url || !filePath) {
-    console.error("Usage: node 5-request_store.js <url> <file_path>");
-    process.exit(1);
-}
-
-request(url, (error, response, body) => {
-    if (error) {
-        console.error(error);
-    } else if (response.statusCode === 200) {
-        fs.writeFile(filePath, body, 'utf-8', (writeError) => {
-            if (writeError) {
-                console.error(writeError);
-            } else {
-                console.log(`Webpage content successfully stored in ${filePath}`);
-            }
-        });
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  } 
+  fs.writeFile(filePath, body, 'utf-8', (writeError) => {
+    if (writeError) {
+      console.error(writeError);
     } else {
-        console.error(`Error: ${response.statusCode}`);
+      console.error(error);
     }
+  });
 });
