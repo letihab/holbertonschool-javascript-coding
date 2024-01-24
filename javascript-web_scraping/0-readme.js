18849e33832f1aa40832892c320400f33905b4cb
letihab <letihabib1822@gmail.com>
2024-01-24 16:28:07 +0100

    update task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -6,19 +6,11 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 
-function readAndPrintFile(filePath) {
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            console.log("File Content:");
-            console.log(data);
-        }
+fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    } else {
+        console.log(data);
+    }
     });
-}
-
-if (!filePath) {
-    console.error("Usage: node read_file.js <file_path>");
-} else {
-    readAndPrintFile(filePath);
-}
