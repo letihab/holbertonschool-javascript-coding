f1154bafaaf5a03abf34f2d4ee75356c39c0dea6
letihab <letihabib1822@gmail.com>
2024-01-24 16:31:21 +0100

    update task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -6,10 +6,10 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-        console.error(err);
-        process.exit(1);
-    } else {
-        console.log(data);
-    }
-    });
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  } else {
+    console.log(data);
+  }
+  });
