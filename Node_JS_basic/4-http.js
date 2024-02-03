266a0b7df50f651aa442359f4f49df11cadbfd5b
letihab <letihabib1822@gmail.com>
2024-02-03 10:57:50 +0100

    update task 4 and 5

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,11 +1,8 @@
-// 4-http.js
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!\n');
-});
-
-app.listen(1245);
-
+  res.writeHead(200, { 'Content-type': 'text/plain' });
+  res.write('Hello Holberton School!');
+  res.end();
+}).listen(1245);
 module.exports = app;
--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,30 +1,23 @@
-// 5-http.js
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
   if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!\n');
   } else if (req.url === '/students') {
-    // Assuming the database file path is passed as a command-line argument
-    const databasePath = process.argv[2];
-    
-    countStudents(databasePath)
+    countStudents(process.argv[2])
       .then(() => {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('This is the list of our students\n');
       })
       .catch((error) => {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end(`Error: ${error.message}\n`);
+        res.end(`This is the list of our students\n${error.message}\n`);
       });
   } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.statusCode = 404;
     res.end('Not Found\n');
   }
-});
-
-app.listen(1245);
+}).listen(1245);
 
 module.exports = app;
