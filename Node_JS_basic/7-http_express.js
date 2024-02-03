c45e30cc990067c27381eaf673d6a0e461b38275
letihab <letihabib1822@gmail.com>
2024-02-03 11:26:36 +0100

    update task 7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,25 +1,24 @@
-// 7-http_express.js
 const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
-const databasePath = process.argv[2];
+const port = 1245;
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!\n');
 });
 
-app.get('/students', async (req, res) => {
-  try {
-    await countStudents(databasePath);
-    res.send('This is the list of our students\n');
-  } catch (error) {
-    res.status(500).send(`Error: ${error.message}\n`);
-  }
+app.get('/students', (req, res) => {
+  countStudents(process.argv[2])
+    .then((result) => {
+      res.send(`This is the list of our students\n${result.sentence1}\n${result.sentence2}\n${result.sentence3}`);
+    })
+    .catch((error) => {
+      res.status(500).send(`This is the list of our students\n${error.message}`);
+    });
 });
 
-app.listen(1245, () => {
-  console.log('Server is listening on port 1245');
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
-
 module.exports = app;
