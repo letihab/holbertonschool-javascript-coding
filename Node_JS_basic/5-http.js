92c2708fe6bf6e11b02ed6a27e579ad4e9aa3440
letihab <letihabib1822@gmail.com>
2024-02-03 11:05:39 +0100

    update task 4 and 5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,24 +1,30 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+const db = process.argv[2];
 
-  if (req.url === '/') {
-    res.end('Hello Holberton School!\n');
-  } else if (req.url === '/students') {
-    countStudents(process.argv[2])
-      .then(() => {
-        res.end('This is the list of our students\n');
+const app = http.createServer((req, res) => {
+  const { url } = req;
+  res.writeHead(200, { 'Content-type': 'text/plain' });
+
+  if (url === '/') {
+    res.write('Hello Holberton School!');
+    res.end();
+  }
+
+  if (url === '/students') {
+    countStudents(db)
+      .then((result) => {
+        res.write('This is the list of our students\n');
+        res.write(`${result.sentence1}\n`);
+        res.write(`${result.sentence2}\n`);
+        res.write(`${result.sentence3}`);
+        res.end();
       })
       .catch((error) => {
-        res.statusCode = 500; // Internal Server Error
-        res.end(`Error: ${error.message}\n`);
+        res.write(`This is the list of our students\n${error.message}`);
+        res.end();
       });
-  } else {
-    res.statusCode = 404; // Not Found
-    res.end('Not Found\n');
   }
 }).listen(1245);
-
 module.exports = app;
