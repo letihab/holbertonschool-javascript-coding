20bda65b45d07fe1506d6c9c29bc6424d2a1a4de
letihab <letihabib1822@gmail.com>
2024-02-03 11:10:27 +0100

    update task 6

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,14 +1,8 @@
-// 6-http_express.js
 const express = require('express');
 
 const app = express();
-
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!\n');
+  res.send('Hello Holberton School!');
 });
-
-app.listen(1245, () => {
-  console.log('Server is listening on port 1245');
-});
-
+app.listen(1245);
 module.exports = app;
