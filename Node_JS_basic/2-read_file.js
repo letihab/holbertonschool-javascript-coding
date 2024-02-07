313795ef1458e8192ff066845582cc82c850c4ad
letihab <letihabib1822@gmail.com>
2024-02-07 09:28:15 +0100

    update task 2

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,45 +1,21 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 
 function countStudents(path) {
-  return fs.readFile(path, 'utf8')
-    .then((data) => {
-      const lines = data.trim().split('\n');
-
-      if (lines.length <= 1) {
-        throw new Error('No valid students in the database');
-      }
-
-      const header = lines[0].split(',');
-      const students = lines.slice(1).map((line) => line.split(','));
-
-      const fieldIndex = header.indexOf('field');
-      if (fieldIndex === -1) {
-        throw new Error('Invalid CSV format: "field" column not found');
-      }
-
-      const fields = {};
-
-      students.forEach((student) => {
-        const [firstname, , , field] = student.map((item) => item.trim());
-
-        if (field in fields) {
-          fields[field].count += 1;
-          fields[field].list.push(firstname);
-        } else {
-          fields[field] = { count: 1, list: [firstname] };
-        }
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const countByField = { CS: [], SWE: [] };
+    const students = data.split('\n');
+    students.filter((line) => line.trim() !== '')
+      .forEach((item) => {
+        const values = item.split(',');
+        const field = values[3].trim();
+        if (field === 'CS' || field === 'SWE') { countByField[field].push(values[0]); }
       });
-
-      console.log(`Number of students: ${students.length}`);
-      for (const field in fields) {
-        if (Object.prototype.hasOwnProperty.call(fields, field)) {
-          const { count, list } = fields[field];
-          console.log(`Number of students in ${field}: ${count}. List: ${list.join(', ')}`);
-        }
-      }
-    })
-    .catch(() => {
-      throw new Error('Cannot load the database');
-    });
+    console.log(`Number of students: ${countByField.SWE.length + countByField.CS.length}`);
+    console.log(`Number of students in CS: ${countByField.CS.length}. List: ${countByField.CS.join(', ')}`);
+    console.log(`Number of students in SWE: ${countByField.SWE.length}. List: ${countByField.SWE.join(', ')}`);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 }
 module.exports = countStudents;
